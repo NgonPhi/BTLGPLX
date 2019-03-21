2df843ccf6391f40abe801c37f89af54d99c846f
namta@winds.vn <123456>
2019-03-21 08:37:51 +0700

    fix giao diện tra cứu luật

--- GPLX/app/src/main/java/com/ngonphikp/gplx/Activity/TCLActivity.java
+++ GPLX/app/src/main/java/com/ngonphikp/gplx/Activity/TCLActivity.java
@@ -18,20 +18,55 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.ngonphikp.gplx.R;
 
 public class TCLActivity extends AppCompatActivity {
     ConstraintLayout itemLXM, itemLOT, itemLXK, itemLXT;
     android.support.v7.widget.Toolbar toolbar;
-
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tcl);
-
         AnhXa();
         ClickItem();
+        SetUPQC();
+        SetUpToolbar();
+    }
+
+    private void SetUpToolbar() {
+        //Set lại title
+        toolbar.setTitle("Luật giao thông");
+        setSupportActionBar(toolbar);
+        //Thêm nút navigation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Lấy chiều cao của ActionBar
+        TypedArray styledAttributes =
+                getTheme().obtainStyledAttributes(new int[] { android.R.attr.actionBarSize });
+        int actionBarSize = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+        //Tạo Drawable mới bằng cách thu/phóng
+        Drawable drawable= ContextCompat.getDrawable(this, R.drawable.back);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        Drawable newdrawable = new BitmapDrawable(getResources(),
+                Bitmap.createScaledBitmap(bitmap, actionBarSize,  actionBarSize, true));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(newdrawable);
+
+        //Bắt sự kiện click nút home
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void SetUPQC() {
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
     }
 
     private void ClickItem() {
@@ -66,9 +101,11 @@ public class TCLActivity extends AppCompatActivity {
         startActivity(intent);
     }
     private void AnhXa() {
+        toolbar = findViewById(R.id.toolbar);
         itemLXM = findViewById(R.id.itemloixemay);
         itemLOT = findViewById(R.id.itemloioto);
         itemLXT = findViewById(R.id.itemloixetai);
         itemLXK = findViewById(R.id.itemloixekhach);
+        viewFlipper =(ViewFlipper)findViewById(R.id.viewFlipper);
     }
 }
